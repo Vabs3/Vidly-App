001714339f22d2f1df2d521deaf8836778983fa2
Pencil <pencil@MacBook-Pro.local>
2020-09-07 21:41:01 +0530

    complete react app ~ Vidlaaaa

---
+++ build/precache-manifest.5319d8b388f2b02aed5c977a6a1175fc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "815bee046e1ac3ea63b3304ee1989935",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fee5d17b909276a272a7",
+    "url": "/static/css/2.43a83f26.chunk.css"
+  },
+  {
+    "revision": "b12989da4efb78a762bc",
+    "url": "/static/css/main.4f71eac5.chunk.css"
+  },
+  {
+    "revision": "fee5d17b909276a272a7",
+    "url": "/static/js/2.c065101c.chunk.js"
+  },
+  {
+    "revision": "d5bc4eb36c6cbe30e823c41375f55b18",
+    "url": "/static/js/2.c065101c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b12989da4efb78a762bc",
+    "url": "/static/js/main.6726803c.chunk.js"
+  },
+  {
+    "revision": "09c4bf2ba5891ec32806",
+    "url": "/static/js/runtime-main.f04e1819.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
